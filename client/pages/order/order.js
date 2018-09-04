0c7ad260807a54d0126ad649e81d8e669afa3812
DUDU <530893915@qq.com>
2018-09-04 21:44:14 +0800

    Add orderList style

--- client/pages/order/order.js
+++ client/pages/order/order.js
@@ -7,7 +7,43 @@ Page({
    */
   data: {
     userInfo: null,
-    locationAuthType: app.data.locationAuthType
+    locationAuthType: app.data.locationAuthType,
+    orderList: [
+      {
+        id: 0,
+        list: [{
+          count: 1,
+          image: 'https://product-1254231096.cos.ap-chengdu.myqcloud.com/product1.jpg',
+          name: '商品1',
+          price: 50.5,
+        }]
+      },
+      {
+        id: 1,
+        list: [{
+          count: 1,
+          image: 'https://product-1254231096.cos.ap-chengdu.myqcloud.com/product1.jpg',
+          name: '商品1',
+          price: 50.5,
+        },
+        {
+          count: 1,
+          image: 'https://product-1254231096.cos.ap-chengdu.myqcloud.com/product1.jpg',
+          name: '商品2',
+          price: 50.5,
+        }
+        ]
+      },
+      {
+        id: 2,
+        list: [{
+          count: 1,
+          image: 'https://product-1254231096.cos.ap-chengdu.myqcloud.com/product1.jpg',
+          name: '商品2',
+          price: 50.5,
+        }]
+      }
+    ], // 订单列表
   },
 
   /**
